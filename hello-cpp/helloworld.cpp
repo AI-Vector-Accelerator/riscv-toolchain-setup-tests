77b9a969def251c6b8b74cc1629d0da0d344d4ca
pa4g17 <pa4g17@soton.ac.uk>
2020-10-22 16:06:20 +0100

    Add working C++ build system
    Add a missing object file at the final linker stage that would
    usually be added with the start files at the final linking
    stage.

--- hello-cpp/helloworld.cpp
+++ hello-cpp/helloworld.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+#include "extra_funcs.h"
+
 int main(void) {
-    std::cout << "Hello World!";
+    std::cout << "Hello World!" << std::endl;
+
+    std::cout << std::endl << "===========================" << std::endl;
+    std::cout << "Separate File Function Call" << std::endl;
+    std::cout << "===========================" << std::endl << std::endl;
+
+    seperate_file_func('g');
+
+    std::cout << std::endl << "===============================" << std::endl;
+    std::cout << "End Separate File Function Call" << std::endl;
+    std::cout << "===============================" << std::endl;
+
+    return 0;
 }
---
+++ hello-cpp/source/extra_funcs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "../header/extra_funcs.h"
+
+int seperate_file_func(char char_to_print) {
+    printf("Printing from a seperate file: %c\n", char_to_print);
+
+    return(6);
+}
+
