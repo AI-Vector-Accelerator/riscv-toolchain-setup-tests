94383d1c80e50956f0b4c8d9be54bf19f8c6ccbd
hc3g17 <hc3g17@soton.ac.uk>
2020-11-13 17:40:27 +0000

    made changes to install script

--- toolchain-setup/install-spike-rvv-0.8.x.sh
+++ toolchain-setup/install-spike-rvv-0.8.x.sh
@@ -2,12 +2,13 @@ export RISCV=/opt/riscv
 export PATH=$PATH:$RISCV/bin
 
 sudo apt update
-sudo apt install device-tree-compiler -y 
+sudo apt install device-tree-compiler -y
 
 git clone https://github.com/riscv/riscv-isa-sim.git
 cd riscv-isa-sim/
 
-git checkout 220ae7ec6b  
+git checkout 220ae7ec6b9bf4a5d81190ffa1028438c3cf402b
+git branch
 
 mkdir -p build
 cd build
