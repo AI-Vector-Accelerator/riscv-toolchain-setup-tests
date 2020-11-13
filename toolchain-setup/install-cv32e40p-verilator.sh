54e354c9fc5c8cd1dc5fc7d1c2a06977f82ca286
hc3g17 <hc3g17@soton.ac.uk>
2020-11-13 16:05:34 +0000

    added multi threading

--- toolchain-setup/install-cv32e40p-verilator.sh
+++ toolchain-setup/install-cv32e40p-verilator.sh
@@ -10,7 +10,7 @@ git clone https://github.com/openhwgroup/core-v-verif.git
 
 
 cd ./core-v-verif/cv32/sim/core
-make    # Clone the CV32E40P, build the Verilator model, run a bare-metal hello-world example
+make -j16    # Clone the CV32E40P, build the Verilator model, run a bare-metal hello-world example
 
 echo ""
 echo "Add $CV32SIM to your enviromental paths using nano ~/.profile"
