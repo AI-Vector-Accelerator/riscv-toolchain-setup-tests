2aebcbad9286087790d9262f946e9277d5aa4cef
hc3g17 <hc3g17@soton.ac.uk>
2020-11-17 13:02:17 +0000

    changed files

--- toolchain-setup/install-spike-rvv-0.8.x_fromGDP42git.sh
+++ toolchain-setup/install-spike-rvv-0.8.x_fromGDP42git.sh
@@ -1,17 +1,10 @@
 export RISCV=/opt/riscv
 export PATH=$PATH:$RISCV/bin
 
-sudo apt update
-sudo apt install device-tree-compiler -y
 
-git clone https://git.soton.ac.uk/gdp42/spike-riscv-isa-sim-with-vector-extensions-v0.8
-cd spike-riscv-isa-sim-with-vector-extensions-v0.8
 
-autoconf
+git clone https://git.soton.ac.uk/gdp42/spike-riscv-vector-extension-v0.8
+ls spike-riscv-vector-extension-v0.8
 
-mkdir -p build
-cd build
 
-../configure --prefix=$RISCV
-make -j16
-sudo make install -j16
+cp spike-riscv-vector-extension-v0.8/* $RISCV/bin
