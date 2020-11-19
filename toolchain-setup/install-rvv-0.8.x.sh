97a2adddf48144d089e002dfec6cdfcf3b198522
hc3g17 <hc3g17@soton.ac.uk>
2020-11-19 16:53:41 +0000

    added echo statements

--- toolchain-setup/install-rvv-0.8.x.sh
+++ toolchain-setup/install-rvv-0.8.x.sh
@@ -9,15 +9,23 @@ sudo apt install git autoconf automake autotools-dev curl python3 libmpc-dev lib
 mkdir -p $RISCV
 cd $RISCV
 
+echo "Starting cloning stage"
 
 git clone -b rvv-0.8.x https://github.com/riscv/riscv-gnu-toolchain
 cd riscv-gnu-toolchain
 git submodule update --init --recursive --progress
 
+echo "Finished cloning stage"
+
+echo "Obtaining different version of new lib"
+
 # Checkout less broken version of newlib
 cd riscv-newlib
 git checkout 3e5302714fae99acc8c439f5870846312d081631
 cd ..
+
+echo "Obtained different version of new lib"
+
 
 echo "Compiling RISC-V vector extension 0.8 Toolchain"
 ./configure --disable-linux --disable-multilib --disable-gdb --prefix=$RISCV --with-arch=rv32imc --with-abi=ilp32
